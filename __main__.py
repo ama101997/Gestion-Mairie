7be0540bb63fc1e238a0e2f5bc78b73721cfa04d
ama101997 <131978987+ama101997@users.noreply.github.com>
2023-04-27 16:58:16 +0200

    Delete __main__.py

--- __main__.py
+++
@@ -1,5 +0,0 @@
-
-  - __init__(self): initialise les variables nécessaires pour l'application
-- start(self): lance l'application
-- use(self): permet à l'utilisateur d'utiliser l'application
-- quit(self): quitte l'application
