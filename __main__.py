382793bb98fd50c2d0809e35de2259df223a40ad
ama101997 <131978987+ama101997@users.noreply.github.com>
2023-04-27 15:25:02 +0200

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,5 @@
+
+  - __init__(self): initialise les variables nécessaires pour l'application
+- start(self): lance l'application
+- use(self): permet à l'utilisateur d'utiliser l'application
+- quit(self): quitte l'application
